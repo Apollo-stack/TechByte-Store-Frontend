3e04d488cf6cb11c95b0538b33d14cb951966670
Ramon <Matheusramon0208@gmail.com>
2025-10-25 22:26:56 -0300

    controle de validação com javaScript feito.

--- js/script.js
+++ js/script.js
@@ -8,16 +8,39 @@ document.addEventListener('DOMContentLoaded', function() {
     if (form) { 
         // 2. Adiciona o event listener ao formulário para o evento 'submit'
         form.addEventListener('submit', function(event) {
+            //seleciona a variavel dentro da funcao
+            const firstNameInput = document.getElementById('fname');
+            const emailInput = document.getElementById('email');
             
-            // 3. IMPEDE o envio padrão do formulário AGORA!
-            event.preventDefault(); 
-            
-            console.log("Formulário submetido! Validação começa agora..."); 
-            
-            // --- A LÓGICA DE VALIDAÇÃO VIRÁ AQUI ---
-            // Por enquanto, vamos só impedir o envio.
+            //pegar os valores e remover os espaços extras
+            const firstNameValue = firstNameInput.value.trim();
+            const emailValue = emailInput.value.trim();
+
+            let isValid = true;
+            let errorMessage = "";
+
+            //verificar se o nome esta vazio
+            if(firstNameValue === ""){
+                isValid = false;
+                errorMessage = "Por favor, preencha o campo 'Primeiro Nome'.";
+            }
+
+            else if(emailValue === "") {
+                isValid = false;
+                errorMessage = "Por favor, preencha o campo 'E-mail'.";
+            }
+
+            if (!isValid) {
+                //se não for válido, impede o envio e mostra o erro
+                event.preventDefault();
+                alert(errorMessage);
+            } else {
+                //se for válido, deixa o envio acontecer
+                console.log("Formulário válido, enviando...")
+            }
             
         });
+        
     } else {
         console.error("Erro: Formulário com id='form1' não encontrado!");
     }
